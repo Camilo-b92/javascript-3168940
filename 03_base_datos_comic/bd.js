b4a5950e55f26100dda5cc5eb5eb5242dea86d28
Camilo Betancourt <camilobetancourt02@gmail.com>
2025-11-05 17:25:19 -0500

    Avance Base de Datos

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,34 @@
+let comic = [
+    {
+  "nombreComic": "Nombre Generico",
+  "numeroEpisodios": "3",
+  "genero": "Ficcion",
+  "sipnosis": "...",
+  "autores": ["Kelos", "Gabriel", "Camilo"],
+  "portada": "imagen.jpg",
+  "year": "2024",
+  "capitulos": [
+    {
+      "id": "1",  
+      "nombre": "El Descubrimiento",
+      "personajes": 10,
+      "portada": "img/molecule.jpg",
+      "descripcion": "Dan Jukes",
+    },
+    {
+      "id": "2",  
+      "nombre": "El De  scubrimiento",
+      "personajes": 10,
+      "portada": "img/molecule.jpg",
+      "descripcion": "Dan Jukes",
+    },
+    {
+      "id": "3",  
+      "nombre": "El Descubrimiento",
+      "personajes": 13,
+      "portada": "img/molecule.jpg",
+      "descripcion": "Dan Jukes",  
+    }
+  ]
+}
+]
