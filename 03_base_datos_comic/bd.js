1a1eb152d33f98ad2e0a3400ae8850f67c134686
Camilo Betancourt <camilobetancourt02@gmail.com>
2025-11-18 17:04:38 -0500

    ESTRUCTURA CAD PERSONAJES

--- 03_base_datos_comic/bd.js
+++ 03_base_datos_comic/bd.js
@@ -1,34 +1,40 @@
-let comic = [
-    {
-  "nombreComic": "Nombre Generico",
+let comic =
+{
+  "nombreComic": "Cuerdas del Destino",
   "numeroEpisodios": "3",
   "genero": "Ficcion",
-  "sipnosis": "...",
+  "sipnosis": "Hola, este es mi comic",
   "autores": ["Kelos", "Gabriel", "Camilo"],
   "portada": "imagen.jpg",
-  "year": "2024",
+  "year": "2025",
+  "personajes": [
+    {
+      "nombre":"Juanes",
+      "descripcion":"Lorem...",
+      "imagen":"img", 
+    }
+  ],
   "capitulos": [
     {
-      "id": "1",  
+      "id": "1",
       "nombre": "El Descubrimiento",
       "personajes": 10,
-      "portada": "img/molecule.jpg",
+      "portada": "Mwuehehehe Cat.jpeg",
       "descripcion": "Dan Jukes",
     },
     {
-          "id": "2",  
-          "nombre": "El De  scubrimiento",
+      "id": "2",
+      "nombre": "El De  scubrimiento",
       "personajes": 10,
-      "portada": "img/molecule.jpg",
+      "portada": "Mwuehehehe Cat.jpeg",
       "descripcion": "Dan Jukes",
     },
     {
-      "id": "3",  
+      "id": "3",
       "nombre": "El Descubrimiento",
       "personajes": 13,
-      "portada": "img/molecule.jpg",
-      "descripcion": "Dan Jukes",  
+      "portada": "Mwuehehehe Cat.jpeg",
+      "descripcion": "Dan Jukes",
     }
   ]
 }
-]
--- 03_base_datos_comic/testComic.js
+++ 03_base_datos_comic/testComic.js
@@ -1,6 +1,8 @@
-const infocomic = document.querySelector(".info-comic")
+const infocomic = document.querySelector(".info-comic");
+const cardPersonajes = document.querySelector(".card.personajes");
 
 console.log (infocomic)  
+console.log (comic.personajes)
 
 infocomic.innerHTML = `
         <small>${comic.year}</small>
@@ -8,3 +10,12 @@ infocomic.innerHTML = `
         <p>${comic.sipnosis}</p>
         <p>Genero: ${comic.genero}</p>
 `
+comic.personajes.forEach(char => {
+      //CREAR ELEMENTOS DINAMICAMENTE CON JAVASCRIPT
+      const div= document.createElement("div")
+      div.classList.add("personajes")
+      
+      console.log(char.nombre)
+      document.body.innerHTML += `<img src="${char.imagen}" width="200"`        
+});
+
